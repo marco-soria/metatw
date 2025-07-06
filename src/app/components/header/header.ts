2f76d76bc499210fe98bc38d8f377023070703b7
marco-soria <marco.soriaa7@gmail.com>
2025-07-06 10:59:50 -0500

    added menu animation

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   imports: [],
-  templateUrl: './header.html'
+  templateUrl: './header.html',
 })
 export class Header {
+  menuQuest = false;
+  menuRayBan = false;
+  menuAsistencia = false;
 
+  onMouseOver(menu: 'quest' | 'rayban' | 'asistencia') {
+    switch (menu) {
+      case 'quest':
+        this.menuQuest = true;
+        break;
+      case 'rayban':
+        this.menuRayBan = true;
+        break;
+      case 'asistencia':
+        this.menuAsistencia = true;
+        break;
+    }
+  }
+
+  onMouseOut(menu: 'quest' | 'rayban' | 'asistencia') {
+    switch (menu) {
+      case 'quest':
+        this.menuQuest = false;
+        break;
+      case 'rayban':
+        this.menuRayBan = false;
+        break;
+      case 'asistencia':
+        this.menuAsistencia = false;
+        break;
+    }
+  }
 }
