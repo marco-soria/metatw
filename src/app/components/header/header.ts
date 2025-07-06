75a2c8248f7b6d94da8ea46bb9a1947fd1a20665
marco-soria <marco.soriaa7@gmail.com>
2025-07-06 11:11:51 -0500

    header update

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -10,7 +10,14 @@ export class Header {
   menuRayBan = false;
   menuAsistencia = false;
 
+  private questTimer: any = null;
+  private raybanTimer: any = null;
+  private asistenciaTimer: any = null;
+
   onMouseOver(menu: 'quest' | 'rayban' | 'asistencia') {
+    // Cancelar cualquier timer pendiente para este menú
+    this.clearTimer(menu);
+
     switch (menu) {
       case 'quest':
         this.menuQuest = true;
@@ -25,15 +32,45 @@ export class Header {
   }
 
   onMouseOut(menu: 'quest' | 'rayban' | 'asistencia') {
+    // Agregar delay de 300ms antes de cerrar el menú
     switch (menu) {
       case 'quest':
-        this.menuQuest = false;
+        this.questTimer = setTimeout(() => {
+          this.menuQuest = false;
+        }, 300);
         break;
       case 'rayban':
-        this.menuRayBan = false;
+        this.raybanTimer = setTimeout(() => {
+          this.menuRayBan = false;
+        }, 300);
         break;
       case 'asistencia':
-        this.menuAsistencia = false;
+        this.asistenciaTimer = setTimeout(() => {
+          this.menuAsistencia = false;
+        }, 300);
+        break;
+    }
+  }
+
+  private clearTimer(menu: 'quest' | 'rayban' | 'asistencia') {
+    switch (menu) {
+      case 'quest':
+        if (this.questTimer) {
+          clearTimeout(this.questTimer);
+          this.questTimer = null;
+        }
+        break;
+      case 'rayban':
+        if (this.raybanTimer) {
+          clearTimeout(this.raybanTimer);
+          this.raybanTimer = null;
+        }
+        break;
+      case 'asistencia':
+        if (this.asistenciaTimer) {
+          clearTimeout(this.asistenciaTimer);
+          this.asistenciaTimer = null;
+        }
         break;
     }
   }
