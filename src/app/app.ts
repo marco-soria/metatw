f8fcd700564a1d0ecf02e56a9684bb0f1a7047c2
marco-soria <marco.soriaa7@gmail.com>
2025-07-06 17:41:23 -0500

    added footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
 import { TopBar } from './components/top-bar/top-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [TopBar, Header],
+  imports: [TopBar, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
