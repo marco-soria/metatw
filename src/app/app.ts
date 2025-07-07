a9b5125b911e9a60ebd25ac2010f2a58e893932a
marco-soria <marco.soriaa7@gmail.com>
2025-07-06 21:00:19 -0500

    added products

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Featured } from './components/featured/featured';
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
+import { Products } from './components/products/products';
 import { TopBar } from './components/top-bar/top-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [TopBar, Header, Footer, Featured],
+  imports: [TopBar, Header, Footer, Featured, Products],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
