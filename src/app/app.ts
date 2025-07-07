e59c2440d2a24a71e7bf51d4a0c02c2384e76315
marco-soria <marco.soriaa7@gmail.com>
2025-07-06 21:22:35 -0500

    added blog

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { Banner } from './components/banner/banner';
+import { Blog } from './components/blog/blog';
 import { Featured } from './components/featured/featured';
 import { Footer } from './components/footer/footer';
+import { Games } from './components/games/games';
 import { Header } from './components/header/header';
 import { Products } from './components/products/products';
 import { TopBar } from './components/top-bar/top-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [TopBar, Header, Footer, Featured, Products, Banner],
+  imports: [TopBar, Header, Footer, Featured, Products, Banner, Games, Blog],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/components/games/games.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-games',
+  imports: [RouterLink],
+  templateUrl: './games.html',
+})
+export class Games {}
