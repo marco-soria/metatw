3eafadea3b752789d7762609ea99cbd4c9ab2287
marco-soria <marco.soriaa7@gmail.com>
2025-07-06 20:52:30 -0500

    added featured

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Featured } from './components/featured/featured';
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
 import { TopBar } from './components/top-bar/top-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [TopBar, Header, Footer],
+  imports: [TopBar, Header, Footer, Featured],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/components/featured/featured.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-featured',
+  imports: [RouterLink],
+  templateUrl: './featured.html',
+})
+export class Featured {}
