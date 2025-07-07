c62a75bcbb48c7c8837fb1589165a81913ee5606
marco-soria <marco.soriaa7@gmail.com>
2025-07-06 21:29:15 -0500

    added newsletter

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,12 +5,23 @@ import { Featured } from './components/featured/featured';
 import { Footer } from './components/footer/footer';
 import { Games } from './components/games/games';
 import { Header } from './components/header/header';
+import { Newsletter } from './components/newsletter/newsletter';
 import { Products } from './components/products/products';
 import { TopBar } from './components/top-bar/top-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [TopBar, Header, Footer, Featured, Products, Banner, Games, Blog],
+  imports: [
+    TopBar,
+    Header,
+    Footer,
+    Featured,
+    Products,
+    Banner,
+    Games,
+    Blog,
+    Newsletter,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/components/newsletter/newsletter.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-newsletter',
+  imports: [RouterLink],
+  templateUrl: './newsletter.html',
+})
+export class Newsletter {}
